e3e289374621b01be6492d88d0fff3ab48b89697
dcuerowe <dcuero@we-techs.com>
2025-12-20 22:59:02 -0300

    Add comment for project delivery in conn_asset.py
    Added a comment indicating project delivery.

--- assets_update/conn_asset.py
+++ assets_update/conn_asset.py
@@ -3,11 +3,13 @@ import os
 import json
 import traceback
 
+# 12052552 Entrega de proyectos
 
 def add_new_options(API_KEY, assets_name):
 
     id_question = ["9d7a30fa-d3ee-c639-7b6f-60be1103c7cc", "7d421fe8-92c8-ddd0-75f2-23ecad62aff1", "5c4c3d6a-933d-c89d-89c1-21b4e0034c23", 
                    "f1de4866-466f-58b9-2b65-d8694d6c71f7", "2b81c480-30a1-25a3-a65d-507f9ba0f441", "a00e9070-96de-15cc-12fd-b90b305f77ff"]
+                   
 
     for id in id_question:
 
