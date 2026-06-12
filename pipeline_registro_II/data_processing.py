2afe2b0b67479153c04b7dccca6b71f28a4f6a5e
dcuerowe <dcuero@we-techs.com>
2026-06-12 13:39:29 -0400

    Se agrega a daniel en las notificaciones

--- pipeline_registro_II/data_processing.py
+++ pipeline_registro_II/data_processing.py
@@ -274,7 +274,7 @@ def inbox(ot, id_tecnico, fecha, id_punto, id_tipo, modelo, serial, trabajo_id, 
         created_inbox,
         f"<b>Caso a ser revisado:</b> {id_tipo} | {id_etiqueta}",
         message_type='notification',
-        partner_ids=[5205, 172],
+        partner_ids=[5205, 172, 5242],
         attachment_ids=[informe] if informe else [])
     
     if id_etiqueta == "Creación en espera":
@@ -283,10 +283,10 @@ def inbox(ot, id_tecnico, fecha, id_punto, id_tipo, modelo, serial, trabajo_id, 
         created_inbox,
         f"<b>Crear equipo en modulo de mantención | Dispositivo instalado:</b> {id_tipo} | S/N: {serial}",
         message_type='notification',
-        partner_ids=[158],
+        partner_ids=[158, 5242],
         attachment_ids=[informe] if informe else [])
         
-        
+         
 
 
 
